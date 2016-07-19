d4fc7be6b5590029a16305fdb3c920652179d46e
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-07-19 12:32:49 +0800

    Synced Healthbar system.

---
+++ Assets/Bullet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet : MonoBehaviour
+{
+	void OnCollisionEnter(Collision collision)
+	{
+		GameObject hit = collision.gameObject;
+		Health health = hit.GetComponent<Health>();
+		if (health != null)
+		{
+			health.TakeDamage(10);
+		}
+
+		Destroy(gameObject);
+	}
+}
---
+++ Assets/Health.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+	public const int maxHealth = 100;
+
+	[SyncVar(hook = "OnChangeHealth")]
+	public int currentHealth = maxHealth;
+	public RectTransform healthBar;
+
+	public void TakeDamage(int amount)
+	{
+		if (!isServer)
+			return;
+
+		currentHealth -= amount;
+		if (currentHealth <= 0)
+		{
+			currentHealth = 0;
+			Debug.Log("Dead!");
+		}
+	}
+
+	void OnChangeHealth(int _health)
+	{
+		healthBar.sizeDelta = new Vector2(_health, healthBar.sizeDelta.y);
+	}
+}
