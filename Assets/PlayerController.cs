2fa707fb73300e42d4029ff3f71cbee696e5be08
Tan Xue Wen, Rayner <raynertanxw@me.com>
2016-07-19 10:24:39 +0800

    Non-network aware shooting.

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -4,6 +4,9 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
 	void Update()
 	{
 		if (!isLocalPlayer)
@@ -14,6 +17,11 @@ public class PlayerController : NetworkBehaviour
 
 		transform.Rotate(0, x, 0);
 		transform.Translate(0, 0, z);
+
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Fire();
+		}
 	}
 
 	public override void OnStartLocalPlayer ()
@@ -21,5 +29,20 @@ public class PlayerController : NetworkBehaviour
 		base.OnStartLocalPlayer ();
 
 		GetComponent<MeshRenderer>().material.color = Color.blue;
+	}
+
+	private void Fire()
+	{
+		// Create the Bullet from the Bullet Prefab
+		var bullet = (GameObject)Instantiate (
+			bulletPrefab,
+			bulletSpawn.position,
+			bulletSpawn.rotation);
+
+		// Add velocity to the bullet
+		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+
+		// Destroy the bullet after 2 seconds
+		Destroy(bullet, 2.0f);
 	}
 }
